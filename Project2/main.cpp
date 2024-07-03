1a2f534b49f60b56e998bf8bf5167414fc677aaf
nsajjala1 <narensr@yahoo.com>
2024-07-03 12:08:59 -0400

    updating Project commit

---
+++ Project2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Project2
+//
+//  Created by Narendranatha Sajjala on 2/19/24.
+//
+
+#include <iostream>
+using namespace std ;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World fro Project 2 " << endl ;
+    return 0;
+}
