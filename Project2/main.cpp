7b8c59889a81cc15269c93646dd5f5153c0b5e83
Naren Sajjala <33432097+nsajjala1@users.noreply.github.com>
2024-07-03 12:15:58 -0400

    Update main.cpp
    updated in GitHub editor

--- Project2/main.cpp
+++ Project2/main.cpp
@@ -10,6 +10,6 @@ using namespace std ;
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World fro Project 2 " << endl ;
+    std::cout << "Hello, World from GitHub " << endl ;
     return 0;
 }
