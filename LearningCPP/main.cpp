75618c52abcff622ba4dc27d69ab83c4dd49de88
Sajjala <narensr@yahoo.com>
2024-07-04 11:59:54 -0400

    commiting from MacBookPro

---
+++ LearningCPP/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  LearningCPP
+//
+//  Created by Narendranatha Sajjala on 7/4/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
