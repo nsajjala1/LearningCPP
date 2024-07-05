25687e59d22c2370c2bb12d2a5ecb3b25a81b6e3
Narendranaha Sajjala <narensr@yahoo.com>
2024-07-05 18:04:33 -0400

    added switch statemetns

--- LearningConditionalStatements/main.cpp
+++ LearningConditionalStatements/main.cpp
@@ -9,55 +9,77 @@
 using namespace std ;
 int main(int argc, const char * argv[]) {
     // insert code here...
- /*
+ 
   // ******if and else *****************
     int time = 20;
+    cout << " IF ELSE statement " << endl ;
     if (time < 18) {
-      cout << "Good day.";
+      cout << "Good day."<<endl;
     } else {
-      cout << "Good evening.\n";
+      cout << "Good evening. "<<endl;
     }
-   // string result = (time < 18) ? "Good day." : "Good evening.";
-   // cout << result
+    string result = (time < 18) ? "Good day." : "Good evening.";
+    cout << result <<endl;
   
   
     // *********if elseif ***********
-    int time = 2;
+    cout << "if and elsif statement " << endl ;
     if (time < 10) {
-      cout << "Good morning.";
+      cout << "Good morning."<<endl;
     } else if (time < 20) {
-      cout << "Good day.";
+      cout << "Good day."<<endl;
     } else {
-      cout << "Good evening.";
+      cout << "Good evening."<<endl;
     }
- */
+ 
     
     
     // **************** SWITCH ***************
+    cout << "SWITCH STATEMENT \n" ;
     int day = 4;
     switch (day) {
       case 1:
-        cout << "Monday";
+        cout << "Monday" <<endl;
         break;
       case 2:
-        cout << "Tuesday";
+        cout << "Tuesday"<<endl;
         break;
       case 3:
-        cout << "Wednesday";
+        cout << "Wednesday"<<endl;
         break;
       case 4:
-        cout << "Thursday";
+        cout << "Thursday"<<endl;
         break;
       case 5:
-        cout << "Friday";
+        cout << "Friday"<<endl;
         break;
       case 6:
-        cout << "Saturday";
+        cout << "Saturday"<<endl;
         break;
       case 7:
-        cout << "Sunday";
+        cout << "Sunday"<<endl;
         break;
     }
+    
+    
+    // ********** WHILE loop ***********
+    cout << "while loop "<<endl ;
+    int i = 0;
+    
+    while (i < 5) {
+      cout << i << "\n";
+      i++;
+    }
+    
+    
+    cout<<"do while loop";
+    i=0;
+    do {
+      cout << i << "\n";
+      i++;
+    }
+    while (i < 5);
+    
     
     
     return 0;
