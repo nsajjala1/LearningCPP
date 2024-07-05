76a75891a09adc8bbdb7c1aaad75f27e589e0c2a
Sajjala <narensr@yahoo.com>
2024-07-05 11:34:33 -0400

    Update main.cpp
    updated in git

---
+++ Test2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Test2
+//
+//  Created by Narendranatha Sajjala on 7/5/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
