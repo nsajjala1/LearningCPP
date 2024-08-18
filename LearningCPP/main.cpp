c8f2d740ca4dbc1196ff83dfea4587ebe1478a83
Narendranaha Sajjala <narensr@yahoo.com>
2024-08-18 19:58:03 -0400

    updated

--- LearningCPP/main.cpp
+++ LearningCPP/main.cpp
@@ -8,51 +8,28 @@
 #include <iostream>
 using namespace std;
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    int age = 2;
-    cout << "the age entered is " << age << endl;
-    if(age > 64 ){
-        cout << " You are old" << endl;
-    } else if (age > 19){
-        cout << " You are an adult " << endl;
-    } else if (age > 12)
-        cout << " you are a teen" << endl;
-    else if ( age > 5)
-        cout << "You are a child" << endl;
-    else if ( age > 2)
-        cout << " you are a toddler" << endl;
-    else
-        cout << "you are infant" << endl;
+
+    cout << " Hello, welcome to Arjun's carpet cleaning service \n";
+    int small_rooms {0};
+    int big_rooms {0};
+    cout << "How many small rooms would you like cleaned?  ";
+    cin >> small_rooms;
+    cout << "How many big rooms would you like cleaned?  ";
+    cin >> big_rooms;
+    const double small_price {25};
+    const double big_price {35};
+    const double tax {0.06};
+    cout << "\n====================================" << endl;
+    cout << "Estimation for carpet cleaning service \n";
+    cout << "Number of small rooms: " << small_rooms << endl;
+    cout << "Number of big rooms: " << big_rooms << endl;
     
-    
-    cout << " Below is switch statement example \n";
-    int day = 9;
-    switch (day){
-        case 1:
-            cout << "Monday"<< endl;
-            break;
-        case 2:
-            cout << "Tuesday" << endl;
-            break;
-        case 3:
-            cout << "Wednesday" << endl;
-            break;
-        case 4:
-            cout << "Thursday" << endl;
-            break;
-        case 5:
-            cout <<"Friday" << endl;
-            break;
-        case 6:
-            cout << "Saturday" << endl;
-            break;
-        case 7:
-            cout << "Sunday" << endl;
-            break;
-        default:
-            cout << "Enter a number 1-7" << endl;
-    }
-    
-    
+    double cost = (small_price * small_rooms) + (big_price * big_rooms);
+    double total_tax = cost * tax;
+    cout << "Cost : $" <<  cost << endl;
+    cout << "Tax : $" << total_tax << endl;
+    cout << "====================================" << endl;
+    cout << " total estimate is $" << cost + total_tax << endl;
+    cout << "this estimate is valid for 30 days  " << endl;
     return 0;
 }
